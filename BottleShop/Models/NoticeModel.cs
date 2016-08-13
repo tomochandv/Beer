7f32a3eaeaaf6796d50487eaa847b66aca9c0b28
tomochan <aka.tomochan@gmail.com>
2016-08-13 23:21:39 +0900

    wewe

---
+++ BottleShop/Models/NoticeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BottleShop
+{
+    public class NoticeModel
+    {
+        public int IDX { get; set; }
+        public string TITLE { get; set; }
+        public string CONTENTS { get; set; }
+        public DateTime INDATE { get; set; }
+    }
+}
